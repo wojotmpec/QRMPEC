73f6ebdba2251e783fa98af63e8d353e956ad787
JerzyGraca <Jerzy.Graca@mpec.krakow.pl>
2022-03-10 13:32:28 +0100

    upd
    upd

--- SynchScreen.js
+++ SynchScreen.js
@@ -15,21 +15,18 @@ function SynchScreen({navigation}) {
       console.log('oky');
 
       var dataArray = Array();
+      var successVal = '0';
       db.transaction(tx => {
    
         tx.executeSql('SELECT * FROM serwis WHERE status = 0', [], (trans, result) => {
           console.log('pp');
           for (let i = 0; i < result.rows.length; ++i) {
             let values = result.rows._array[i];
-  //          {id:1,name:'aaa'},{id:2,name:'bbb'}
-//            valService = 'rodzaj:' + values['rodzaj'] + ',wID:' + values['w_id'] + '&serwisID=' + values['serwis_id'] + '&opis=' + values['opis'] + '&status=' + values['status'] + '&timeService=' + values['Timestamp']
             dataArray.push({rodzaj: values['rodzaj'], wID: values['w_id'], serwisID: values['serwis_id'], opis: values['opis'], status: values['status'], timeService: values['Timestamp']})
-      //      console.log(val)
           }
           console.log(dataArray);
            
-  
-            let res = fetch('http://hercules8.mpec.krakow.pl/postCreate.php', {
+              let res = fetch('http://hercules8.mpec.krakow.pl/postCreate.php', {
               method: 'POST',
               headers: {
                 Accept: 'application/json',
@@ -37,28 +34,42 @@ function SynchScreen({navigation}) {
               },
               body: JSON.stringify({
                 dataArray
-//                 'val': 'KKK-JG'
                  })        
             })
             .then(res => res.json())
             .then(data => {
   //            console.log(data);
-              // enter you logic when the fetch is successful
-               console.log( data.testJG)
-
+              // enter you logic when the fetch is successful               
+               successVal = data.status
+               console.log( 'PPR'+ successVal)
+               db.transaction(tx1 => {
+   
+                console.log( 'HERE' + successVal)
+                if(successVal == '1') {
+                  tx1.executeSql(
+                    'UPDATE serwis SET status=1 WHERE status = 0;'
+                  )
+                  console.log( 'DONE')
+                } else {
+                  console.log( 'FAILED')
+                }        
+              }) 
+               console.log( 'EEE'+ successVal)
              }).catch(error => {
               // enter your logic for when there is an error (ex. error toast)
                console.log(error)
              }) 
   
-  
-  
-        });  
-      })
-  
-  
+        })
+      }) 
+
+
+
     }
       
+
+  
+
     return (
       <View style={{ flex: 0, alignItems: 'center', justifyContent: 'center', fontSize: 40 }}>      
       <Text>{"\n"}{"\n"}</Text>
